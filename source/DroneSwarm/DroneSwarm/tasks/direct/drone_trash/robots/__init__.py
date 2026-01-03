2c4f5f51f77caf2d6a0ead8ff09067715cc66ec8
Teodor Bogoeski <teodorbogoeski@gmail.com>
2026-01-03 16:39:26 +0100

    feat: implement Drone Trash Collection environment with agent and configuration files

---
+++ source/DroneSwarm/DroneSwarm/tasks/direct/drone_trash/robots/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022-2025, The Isaac Lab Project Developers.
+# All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+
+"""Robot configurations for drone trash collection."""
+
+from .quadcopter import CRAZYFLIE_CFG
